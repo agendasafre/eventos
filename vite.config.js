23367335e7df592bbe964bb23d81321ddd6dcf66
petro232 <andpetronio@gmail.com>
2025-10-27 16:52:59 -0300

    auto deploy

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         manual: resolve(__dirname, 'manual.html'),
         quien: resolve(__dirname, 'quien.html'),
         retirados: resolve(__dirname, 'retirados.html'),
+        pendientes: resolve(__dirname, 'pendientes.html'),
       },
     },
   },
